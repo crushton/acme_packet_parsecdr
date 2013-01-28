a25f1e19cbbf439f6749877bbdf000e2b5a4259d
Chris Rushton <myst3k@gmail.com>
2013-01-28 16:27:29 -0500

    added C610 start record

--- createdb.pl
+++ createdb.pl
@@ -23,6 +23,7 @@ use v5.10;
 use DBI;
 
 my @completed_versions = (
+    { name => "C610", type => "start" },
     { name => "C620", type => "start" },
     { name => "C620", type => "stop" },
     { name => "C630", type => "start" },
